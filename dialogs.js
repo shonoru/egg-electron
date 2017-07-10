8ea2ff87062438b9fb159d6d2aa52c1950408811
Andrey Bodoev <shonoru@gmail.com>
2017-07-11 06:44:11 +0800

    Add save and open dialogs

--- dialogs.js
+++ dialogs.js
@@ -2,7 +2,7 @@ const { dialog, app, nativeImage } = require('electron');
 const fs = require('fs');
 const path = require('path');
 
-module.exports = { showMessage };
+module.exports = { showMessage, showSaveDialog, showOpenDialog };
 
 function showMessage(browserWindow) {
     dialog.showMessageBox(browserWindow, {
@@ -15,4 +15,30 @@ function showMessage(browserWindow) {
     }, clickedIndex => {
       console.log(clickedIndex)
     });
+}
+
+function showSaveDialog(browserWindow) {
+  dialog.showSaveDialog(browserWindow, {
+    defaultPath: path.join(app.getPath('downloads'), 'memory-info.txt')
+  }, filename => {
+    const memInfo = JSON.stringify(process.getProcessMemoryInfo(), null, 2);
+    fs.writeFile(filename, memInfo, 'utf8', err => {
+      if (err) {
+        dialog.showErrorBox('Save Failed.', err.message);
+      }
+    })
+  })
+}
+
+function showOpenDialog(browserWindow) {
+  dialog.showOpenDialog(browserWindow, {
+    defaultPath: app.getPath('downloads'),
+    filters: [
+      { name: 'Text Files', extensions: ['txt'] }
+    ]
+  }, filepaths => {
+    if (filepaths) {
+      console.log(filepaths, fs.readFileSync(filepaths[0], 'utf8'));
+    }
+  })
 }
--- main-menu.js
+++ main-menu.js
@@ -1,6 +1,6 @@
 const { app, Menu } = require('electron');
 const isWindows = process.platform === 'win32';
-const { showMessage } = require('./dialogs.js');
+const { showMessage, showSaveDialog, showOpenDialog } = require('./dialogs.js');
 
 module.exports = {
   setMainMenu
@@ -40,6 +40,18 @@ function setMainMenu(mainWindow) {
           label: 'Say Hello',
           click() {
             showMessage(mainWindow);
+          }
+        },
+        {
+          label: 'Save Memory Usage Info',
+          click() {
+            showSaveDialog(mainWindow);
+          }
+        },
+        {
+          label: 'Open File',
+          click() {
+            showOpenDialog(mainWindow);
           }
         }
       ]
