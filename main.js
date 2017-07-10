f9aec6306263d07532bdfca521338ac861b6c14c
Andrey Bodoev <shonoru@gmail.com>
2017-07-11 07:17:10 +0800

    Add ipcMain to create new windows

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, ipcMain } = require('electron');
 const path = require('path');
-let mainWindow;
+let mainWindow, windows = [];
 const { setMainMenu } = require('./main-menu.js');
 
 app.on('ready', () => {
@@ -13,4 +13,20 @@ app.on('ready', () => {
   });
 
   setMainMenu(mainWindow);
+
+  ipcMain.on('create-window', (event, props) => createBrowserWindow(props));
 });
+
+function createBrowserWindow(browserWindowOpts) {
+  let win = new BrowserWindow(Object.assign({
+    width: 400,
+    height: 300
+  }, browserWindowOpts));
+
+  windows.push(win);
+  win.loadURL(path.join('file://', __dirname, 'index.html'));
+
+  win.on('close', () => {
+    windows.splice(windows.indexOf(win), 1);
+  })
+}
--- renderer.js
+++ renderer.js
@@ -1,3 +1,12 @@
+const { ipcRenderer } = require('electron');
+
+document.querySelector('#new-window').addEventListener('click', () => {
+  ipcRenderer.send('create-window', {
+    x: 0,
+    y: 0
+  });
+})
+
 const versionEl = document.querySelector('#version');
 versionEl.innerText = process.versions.electron;
 console.log(process.versions)
